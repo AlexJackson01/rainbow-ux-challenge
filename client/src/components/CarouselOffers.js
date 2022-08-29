1f6902634b3c10c6e581f0fc201548631be10236
AlexJackson01 <ajjackson22@gmail.com>
2022-08-29 21:35:36 +0100

    Tweaked position of carousel controls

--- client/src/components/CarouselOffers.js
+++ client/src/components/CarouselOffers.js
@@ -1,7 +1,11 @@
-import Greggs from '../images/greggs.png';
-import Slimming from '../images/slimming.png';
-import School from '../images/school.png';
+import Greggs from '../images/Greggs.png';
+import Slimming from '../images/Slimming.png';
+import School from '../images/School.png';
+// import Play from '../images/play.png';
+// import Pause from '../images/pause.png';
 import $ from 'jquery';
+import { Play, Pause } from 'react-bootstrap-icons';
+
 
 
 export default function CarouselOffers() {
@@ -9,61 +13,60 @@ export default function CarouselOffers() {
 
   return (
     <div>
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+{/* <div className="carousel-controls"> */}
+<div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+    <div className="carousel-controls">
+  <div className="carousel-indicators">
+  {/* <button id="play-button" type="button" class="btn btn-default btn-xs">
+  <Play className="control-button-icon" size={17} /> 
+ </button>     */}
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    {/* <button id="pause-button" type="button" class="btn btn-default btn-xs">
+    <Pause className="control-button-icon" size={17} />    
+    </button>   */}
+    </div>
+    
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
   </div>
+  {/* </div> */}
   <div className="carousel-inner">
     <div className="carousel-item active">
-    <div className="d-flex">
-    <div className="carousel-card">
-            <div className="carousel-content">
-                <h2>Greggs Offers</h2>
-                <p>Stock up your freezer with your Greggs favourites.</p>
-                <p>​Iceland’s exclusive range of Greggs pasties, bakes, pies and sausage rolls allows you to transform your kitchen into your own personal bakery.</p>
-                <button type="button" class="carousel-btn btn btn-light">Shop Greggs</button>
-            </div>
-        </div>
-        <img src={Greggs} className="carousel-image w-50" alt="Greggs steak bake with any 3 for £5 deal" />
-        </div>
+      <img src={Greggs} className="d-block w-100" alt="Greggs steak bake with any 3 for £5 deal" />
+      <div class="carousel-caption">
+        <h2>Greggs Offers</h2>
+        <p>Stock up your freezer with your Greggs favourites.</p>
+        <p>​Iceland’s exclusive range of Greggs pasties, bakes, pies and sausage rolls allows you to transform your kitchen into your own personal bakery.</p>
+        <button type="button" class="carousel-btn btn btn-light">Shop Greggs</button>
+      </div>
     </div>
     <div className="carousel-item">
-        <div className="d-flex">
-        <div className="carousel-card card-2">
-            <div className="carousel-content">
-                <h2>Slimming World</h2>
-                <p>We’ve teamed up with Slimming World to create a huge selection of healthy foods so you can enjoy without the guilt. From quarter pounders to cod-cakes - our selection can be enjoyed with our Slimming World chips.</p>
-                <p>​Try Iceland’s exclusive range of low-calorie dishes to help keep you on track. </p>
-                <button type="button" class="carousel-btn btn btn-light">Shop Slimming World</button>
-            </div>
-        </div>
-        <img src={Slimming} className="carousel-image w-50" alt="Slimming world potato wedges with any 3 for £6 deal" />
-        </div>
+      <img src={Slimming} className="d-block w-100" alt="Slimming World potato wedges with any 3 for £6 deal" />
+      <div class="carousel-caption">
+        <h2>Slimming World</h2>
+        <p>We’ve teamed up with Slimming World to create a huge selection of healthy foods so you can enjoy without the guilt. From quarter pounders to cod-cakes - our selection can be enjoyed with our Slimming World chips.</p>
+        <p>​Try Iceland’s exclusive range of low-calorie dishes to help keep you on track.</p>
+        <button type="button" class="carousel-btn btn btn-light">Shop Slimming World</button>
+      </div>
     </div>
     <div className="carousel-item">
-        <div className="d-flex">
-        <div className="carousel-card card-3">
-            <div className="carousel-content">
-                <h2>Back to School Essentials</h2>
-                <p>We’ve got all your school shop essentials to kickstart your star pupil’s day.</p>
-                <p>Breakfast, lunch and dinner is covered with our deals on cereals, porridge packs, bitesize snacks and teatime essentials. Check out our offers today!</p>
-                <button type="button" class="carousel-btn btn btn-light">Shop School Essentials</button>
-            </div>
-        </div>
-        <img src={School} className="carousel-image w-50" alt="Woman and child making lunch with any 2 for £2.50 deal" />
-        </div>
+      <img src={School} className="d-block w-100" alt="Woman and child making lunch with any 2 for £2.50 deal" />
+      <div class="carousel-caption">
+        <h2>Back to School Essentials</h2>
+        <p>We’ve got all your school shop essentials to kickstart your star pupil’s day.</p>
+        <p>Breakfast, lunch and dinner is covered with our deals on cereals, porridge packs, bitesize snacks and teatime essentials. Check out our offers today!</p>
+        <button type="button" class="carousel-btn btn btn-light">Shop School Essentials</button>
+      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
 </div>
   )
--- client/src/components/Deals.js
+++ client/src/components/Deals.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Deals() {
   return (
-    <div className='container'>
+    <div className='spacing container'>
         <h1>Browse Deals</h1>
     </div>
   )
